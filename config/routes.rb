eb0c9ba22388a9a2328f88b2c297a635907239d9
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-10 09:43:07 -0600

    added sessions folder and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'sessions#home'
   get '/signup' => 'users#new'
   #post 'signup' => 'users#create' (use built in submission instead)
   # custom routes above line
