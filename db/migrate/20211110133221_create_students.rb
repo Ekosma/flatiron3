bcf53dc1cbefbb71a696a01d4a1861206c360295
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-10 07:32:36 -0600

    student Model Added

---
+++ db/migrate/20211110133221_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :student_name
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
