8da09e669f3f434882a1d41d94e069ad75aff8a2
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-10 09:32:21 -0600

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user_id
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  
+  def user_params
+    params.require(:user)permit(:username, :email, :password)
   end
 
 end
