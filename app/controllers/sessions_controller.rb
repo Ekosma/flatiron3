3314fe4d1739296cae85ad408ab37494d6224fa8
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-12 15:08:13 -0600

    updated some ccs, edited pathways, removed home link, added Account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to period_path(@user)
+      redirect_to periods_path(@user)
     else
       flash[:message] = "Incorrect login, please try again"
       redirect_to 'sessions/new'
