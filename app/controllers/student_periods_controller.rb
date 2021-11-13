8ab9f3f0844e6075fff3f2a83f30c3d8299195d0
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-13 14:50:18 -0600

    added student_period files. Added join table create method

--- app/controllers/student_periods_controller.rb
+++ app/controllers/student_periods_controller.rb
@@ -1,2 +1,16 @@
 class StudentPeriodsController < ApplicationController
+
+  def create
+    @student_period = current_user.students_periods.build(student_period_params)
+    if @student_period.save
+      redirect_to periods_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def student_period_params
+    params.require(:student_period).permit(:period_id, :student_id)
 end
