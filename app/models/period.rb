2199847605d3d00ad03e3e01f18a58d9b1949cc0
Evelyn <evelyn.a.kosma@gmail.com>
2021-11-18 09:22:34 -0600

    fixed model relationships

--- app/models/period.rb
+++ app/models/period.rb
@@ -2,4 +2,5 @@ class Period < ApplicationRecord
   belongs_to :user
   has_many :students
   has_many :assignments
+  has_many :grades, through: :assignments
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,6 @@ class Student < ApplicationRecord
   belongs_to :user
   has_many :assignments
   has_many :periods
-  has_many :grades, dependant: :destroy 
+  has_many :grades, dependant: :destroy
+  has_many :assignments, through: :grades
 end
